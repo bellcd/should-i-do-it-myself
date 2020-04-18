815b1258e8d7839e6f72ddebe1ed12b11ef97151
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-04-18 16:08:04 -0400

    Adds basic setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,57 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Formik, Form, Field } from 'formik';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default IndexPage
+    this.state = {
+      name: '',
+      yearlyIncome: 0,
+      hoursInAYear: 8760,
+      ratePerHourAlive: null
+    };
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <Formik
+          initialValues={{
+            name: '',
+            yearlyIncome: 0,
+          }}
+
+          onSubmit={(values, { setSubmitting }) => {
+            this.setState({ ...values });
+            setSubmitting(false);
+          }}
+        >
+          <Form>
+            <label htmlFor="name">Name</label>
+            <Field name="name" type="text" />
+
+            <label htmlFor="yearlyIncome">Yearly Income (after tax)</label>
+            <Field name="yearlyIncome" type="number" step={1} />
+
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+
+
+        <div>Your current mix of activities (working, eating, sleeping, relaxing, etc...) provides you ${(this.state.yearlyIncome / this.state.hoursInAYear).toFixed(2)} for every hour that you're alive.</div>
+
+
+        <h2>Some Thoughts</h2>
+        <div>Does that mean your </div>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )
+  }
+}
